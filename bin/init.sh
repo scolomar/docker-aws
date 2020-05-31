ecace40c783741d463479b1361fc0bd3f37b62f4
secobau <sebastian.colomar@gmail.com>
2020-05-31 16:20:05 +0200

    Update init.sh

--- bin/init.sh
+++ bin/init.sh
@@ -7,12 +7,26 @@
 set +x && test "$debug" = true && set -x				;
 #########################################################################
 test -n "$A"	                && export A                 || exit 100 ;
+test -n "$apps" 		&& export apps	            || exit 100	;
+test -n "$branch_app" 	        && export branch_app	    || exit 100	;
 test -n "$branch_docker_aws"    && export branch_docker_aws || exit 100 ;
 test -n "$debug" 		&& export debug	            || exit 100	;
 test -n "$domain" 		&& export domain	    || exit 100	;
 test -n "$HostedZoneName"	&& export HostedZoneName    || exit 100 ;
+test -n "$Identifier"		&& export Identifier        || exit 100 ;
+test -n "$KeyName"              && export KeyName	    || exit 100	;
 test -n "$mode"                 && export mode	            || exit 100	;
+test -n "$repository_app"       && export repository_app    || exit 100	;
+test -n "$RecordSetName1"	&& export RecordSetName1    || exit 100 ;
+test -n "$RecordSetName2"	&& export RecordSetName2    || exit 100 ;
+test -n "$RecordSetName3"	&& export RecordSetName3    || exit 100 ;
+test -n "$s3name"		&& export s3name    	    || exit 100 ;
+test -n "$s3region"		&& export s3region    	    || exit 100 ;
 test -n "$stack"                && export stack	            || exit 100	;
+test -n "$template"		&& export template    	    || exit 100 ;
+test -n "$TypeManager"		&& export TypeManager 	    || exit 100 ;
+test -n "$TypeWorker"		&& export TypeWorker 	    || exit 100 ;
+test -n "$username_app"         && export username_app	    || exit 100	;
 #########################################################################
 file=common-functions.sh						;
 path=$A/lib								;
