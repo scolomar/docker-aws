ee242c40a1ae5cef0a6a0dcfd15bcd89b354477b
secobau <sebastian.colomar@gmail.com>
2020-06-24 16:05:11 +0200

    Update cluster-kubernetes-worker.sh

--- bin/cluster-kubernetes-worker.sh
+++ bin/cluster-kubernetes-worker.sh
@@ -7,6 +7,7 @@
 set +x && test "$debug" = true && set -x				;
 #########################################################################
 test -n "$debug"                || exit 100                             ;
+test -n "$ip_leader"   		|| exit 100                             ;
 test -n "$kube"    		|| exit 100                             ;
 test -n "$log"                	|| exit 100                             ;
 test -n "$token_discovery"      || exit 100                             ;
@@ -27,7 +28,7 @@ token_token="$(								\
 		--decode						\
 )"							         	;
 #########################################################################
-#########################echo $ip $kube | tee --append /etc/hosts                           	;
+echo $ip_leader $kube | tee --append /etc/hosts                        	;
 #########################################################################
 while true								;
 do									\
